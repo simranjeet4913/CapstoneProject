22a9726aa6de026ecb8f05a3cb914165977aaa25
RajdeepKaur3105 <rajdeep.rkb@gmail.com>
2020-10-30 11:27:09 -0400

    Done by Rajdeep

---
+++ about.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>
+            Delta English School --About
+        </title>
+        <link rel = "stylesheet" type="text/css" href="about.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+        
+             </head>
+    <body>
+    <img src="images/about.jpg" height="170px" width="100%"/>
+    <div class = "part1"> 
+    <h3> WHO WE ARE?</h3>
+    <p class = "P1"> Our school synonymous with th motto of  Enlightenment with education! Within a short span of few years, Delta English School Jalandhar has scaled great heights in every field. The school is thriving with four wings; Pre-Primary, Primary, Senior and Senior Secondary.Presently, the school has on roll above 1900 students who are being nurtured to excel academically, intellectually and spiritually. A variety of co-curricular activities enable the students to develop a healthy global outlook. Morever, our students are being instructed and guided through the latest methods and techniques in the field of science and are information technology savvy, with strong moral and ethical values. The school is a powerful strategic tool to meet the students and parents rising expectations in the challenging knowledge environment.
+    A galaxy of dedicated teachers imparts quality education ensuring all round development of the children. Every year a number of children qualify for admissions in different professional colleges in India and abroad taking with them and upholding the motto of the school “Service Before Self”.</p> </div>
+    <div class = "part2">
+        <marquee> A school synonymous with excellence in education! Within a short span of few years, Delhi Public School Jalandhar has scaled great heights in every field. The school is thriving with four wings; Pre-Primary, Primary, Senior and Senior Secondary. The mission of our institution is to enlighten our students; empowering them with high standards of scholastic and co-scholastic excellence; to lead the nurturance of the next decade knowledge, skills, values and attitudes in them to ensure their self-actualization and thereby becoming living examples of our motto “Service Before Self”. </marquee>
+        </div>
+    
+        <div class="part3">
+            <div class="part3-1">
+                <div class = "icon"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> </div>The mission of our institution is to enlighten our students; empowering them with high standards of scholastic and co-scholastic excellence; to lead the nurturance of the next decade knowledge, skills, values and attitudes in them to ensure their self-actualization and thereby becoming living examples of our motto “Service Before Self”.</div>
+             
+             <div class="part3-1">
+                  <div class = "icon"><i class="fa fa-line-chart" aria-hidden="true"></i> </div> 
+                 ‘Service Before Self’ – Our motto is a vivid reflection of the mettle that goes into the making of an institution or an organization. Our motto is a constant reminder that we should serve others willingly, graciously and selflessly. We should not expect anything in return for our service because there is a greater pleasure in giving than in receiving. We believe in the saying, ‘Thy need is greater than mine’.</div>
+             <div class="part3-1">
+                 <div class = "icon">  <i class="fa fa-smile-o" aria-hidden="true"></i></div>
+We believe in giving wings to our children and for this we are consistently in pursuit of creating a happy child who looks forward to come to school, to read, to play, to dance, to sing and in reality to learn. Our guiding philosophy is – we nurture young minds by providing sound teaching foundation. Our ultimate aim is NO CHILD LEFT BEHIND. </div>
+        
+        </div>
+    </body>
+
+</html>
+
